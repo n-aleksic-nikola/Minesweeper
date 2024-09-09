217384c8b1d5eb880a08b6a3caf374901aff85c4
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2024-09-09 12:27:46 +0200

    Add view

--- Minesweeper/Model/Board.cs
+++ Minesweeper/Model/Board.cs
@@ -2,8 +2,15 @@
 {
     public class Board
     {
-        public Cell[,] Cells { get; set; }
+        public Cell[,] Cells;
         public int Width { get; set; }
         public int Height { get; set; }
+
+        public Board(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Cells = new Cell[width, height];
+        }
     }
 }
---
+++ Minesweeper/View/ConsoleView.cs
@@ -0,0 +1,18 @@
+﻿using Minesweeper.Model;
+
+namespace Minesweeper.View
+{
+    public class ConsoleView
+    {
+        public void DisplayBoard(Board board)
+        {
+            for (int x = 0; x < board.Width; x++)
+            {
+                for (int y = 0; y < board.Height; y++)
+                {
+                    Console.Write("# ");
+                }
+            }
+        }
+    }
+}
